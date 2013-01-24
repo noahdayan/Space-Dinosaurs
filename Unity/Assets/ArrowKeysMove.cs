a1fb69713b615be3c62bf0a4d8a20dd97fdb5e06
Javier Pérez <javierp@lavabit.com>
2013-01-24 18:00:36 -0500

    Now adding the scripts.

---
+++ Unity/Assets/ArrowKeysMove.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class ArrowKeysMove : MonoBehaviour {
+
+	public float speed = 2.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.position;
+		
+		// Time.deltaTime corrects for errors with the framerate, otherwise it's a pain.
+		newPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		newPosition.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		transform.position = newPosition;
+	}
+}
---
+++ Unity/Assets/ClickAndMove.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class SelectAndMove : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+		// Print out the starting position of the object to the console (at instantiation)
+		Debug.Log ("Starting position: ");
+		Debug.Log (transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// True when the player clicks the left mouse button.
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Left mouse button pressed.");
+			Vector3 newPosition = transform.position;
+			
+			// Get mouse x and y values
+			newPosition.x = Input.mousePosition.x;
+			newPosition.y = Input.mousePosition.y;
+			
+			// Update location of object with the coordinates from above.
+			transform.position = newPosition;
+			Debug.Log (transform.position);
+		}
+	}
+}
