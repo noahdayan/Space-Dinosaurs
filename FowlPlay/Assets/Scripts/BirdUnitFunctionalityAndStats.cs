ad525c71c4b768d592be38fb6ebd7afe0ccf9244
Matthew Adler <madler72@gmail.com>
2013-02-08 17:02:33 -0500

    The scripts were actually added in this one.... player, and unit functionality begun

---
+++ FowlPlay/Assets/Scripts/BirdUnitFunctionalityAndStats.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class BirdUnitFunctionalityAndStats : MonoBehaviour {
+
+	public int healthPoints, defensePoints, attackPoints;
+	public int moveCost;
+	public int tamePower;
+	
+	
+	/**
+	 * Has this unit take damage, usually called by another unit's "AttackUnit" function
+	 * 
+	 * @param dmg the ammount of damage to be dealt
+	 * @return returns the remaining health of this unit after the attack.
+	 */
+	public int TakeDamage(int dmg)
+	{
+		if (dmg - defensePoints > 0)
+		{
+			healthPoints -= dmg - defensePoints;
+		}
+		return healthPoints;
+	}
+	
+	public void MovementCost (GameObject player)
+	{
+		player.SendMessage("RemoveAp", moveCost);
+	}
+	
+	public void AttackUnit (GameObject unit)
+	{
+		unit.SendMessage("TakeDamage", attackPoints);
+		//maybe remove AP here as well based on an attack cost?
+	}
+	
+	public void TameUnit (GameObject unit)
+	{
+		unit.SendMessage("AddTamePointsByRate", tamePower);
+		//Maybe remove AP from the player here as well based on the tame cost?
+	}
+	
+	
+	
+}
---
+++ FowlPlay/Assets/Scripts/DinosaurUnitFunctionalityAndStats.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class DinosaurUnitFunctionalityAndStats : MonoBehaviour {
+	
+	public int healthPoints, defensePoints, attackPoints;
+	public float tamePoints;
+	public int moveCost;
+	public float tameRate;
+	public int tameTickAmount;
+	
+	
+	/**
+	 * Has this unit take damage, usually called by another unit's "AttackUnit" function
+	 * 
+	 * @param dmg the ammount of damage to be dealt
+	 * @return returns the remaining health of this unit after the attack.
+	 */
+	public int TakeDamage(int dmg)
+	{
+		if (dmg - defensePoints > 0)
+		{
+			healthPoints -= dmg - defensePoints;
+		}
+		return healthPoints;
+	}
+	
+	public float EndTurnTickUntame (int tameRate, int commanderDistance)
+	{
+		tamePoints -= (tameTickAmount * commanderDistance);
+		return tamePoints;
+	}
+	
+	public void AttackUnit (GameObject unit)
+	{
+		unit.SendMessage("TakeDamage", attackPoints);		
+	}
+	
+	public float AddTamePointsByRate (int tameAmount)
+	{
+		tamePoints += tameAmount * tameRate;
+		return tamePoints;
+	}
+	
+}
