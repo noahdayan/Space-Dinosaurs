e8903c1b0e3e91b966bf008ee4d2c98ff4e62d45
Noah Dayan <noahdayan@gmail.com>
2013-02-11 23:16:46 -0500

    added camera movement range

--- FowlPlay/Assets/Scripts/ArrowKeysMove.cs
+++ FowlPlay/Assets/Scripts/ArrowKeysMove.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class ArrowKeysMove : MonoBehaviour {
 
-	public float speed = 2.0f;
+	public float speed = 20.0f;
+	public float scrollSpeed = 100.0f;
+	public Vector2 xRange;
+	public Vector2 yRange;
+	public Vector2 zRange;
 	
 	// Use this for initialization
 	void Start () {
@@ -13,12 +17,34 @@ public class ArrowKeysMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 newPosition = transform.position;
+		Vector3 tempPos = transform.position;
+		float temp;
 		
 		// Time.deltaTime corrects for errors with the framerate, otherwise it's a pain.
-		newPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		newPosition.z += Input.GetAxis("Vertical") * speed * Time.deltaTime;
-		newPosition.z += Input.GetAxis("Mouse ScrollWheel") * speed * 50 * Time.deltaTime;
-		newPosition.y -= Input.GetAxis("Mouse ScrollWheel") * speed * 50 * Time.deltaTime;
+		temp = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		tempPos.x += temp;
+		if(xRange.y < tempPos.x && tempPos.x < xRange.x)
+		{
+			newPosition.x += temp;
+		}
+		temp = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		tempPos.z += temp;
+		if(zRange.y < tempPos.z && tempPos.z < zRange.x)
+		{
+			newPosition.z += temp;
+		}
+		temp = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
+		tempPos.z += temp;
+		if(zRange.y < tempPos.z && tempPos.z < zRange.x)
+		{
+			newPosition.z += temp;
+		}
+		temp = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
+		tempPos.y -= temp;
+		if(yRange.y < tempPos.y && tempPos.y < yRange.x)
+		{
+			newPosition.y -= temp;
+		}
 		transform.position = newPosition;
 	}
 }
