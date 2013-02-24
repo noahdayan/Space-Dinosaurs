8e0f55b261223e570fd51db6a85f1ee7de74ead7
Matthew Adler <madler72@gmail.com>
2013-02-24 18:31:36 -0500

    Upgraded the block game!

--- FowlPlay/Assets/Scripts/ActionEventScripts/BarGrowAndHit.cs
+++ FowlPlay/Assets/Scripts/ActionEventScripts/BarGrowAndHit.cs
@@ -3,45 +3,73 @@ using System.Collections;
 
 public class BarGrowAndHit : MonoBehaviour {
 	
+	public GameObject blockChild;
+	
 	int speed = 30;
 	float t = 0f;
 	bool growing = true;
 	bool stopped = false;
 	Vector3 currentScale;
 	Vector3 newScale;
+	public float flashRate = 0.5f;
+	
+	private Color originalColor;
+	private Color currentColor;
 	
 	// Use this for initialization
 	void Start () {
+		originalColor = Color.white;
+		blockChild.renderer.material.color = originalColor;
 		//Grow();
 	}
 	
 	
+/*	8 - 11 red
+	6 - 8 yellow
+	5 - 6 green
+	3 - 5 yellow
+	0 - 3 red */
 	
 	
-	//GREEN IS CURRENTLY between 6 and 7!
+	
 	
 	// Update is called once per frame
 	void Update () {
 		
 		currentScale = gameObject.transform.localScale;
 		
-		if (Input.GetButtonDown("Fire1"))// && !stopped)
+		
+		//Maybe get the difference of the absolute value of the current scale and 5.5. Then divide by that.
+		//If between 5 and 6, it's actually a multiplication. Other wise it's less damage!
+		if (Input.GetButtonDown("Fire1") && !stopped)
 		{
-			stopped = true;
+			//stopped = true;
+			Debug.Log("The current scale is: " + currentScale.y);
+			StopCoroutine("Return");
+			currentColor = BlockManager.CheckPosition((int) currentScale.y).renderer.material.color;
+			blockChild.renderer.material.color = currentColor;
+			BlockManager.Shuffle();
+			BlockManager.PlaceBlocks();
+			//LERPING IS TAKING TOO LONG :(
+			//blockChild.renderer.material.color = Color.magenta;
+			StartCoroutine("Return");
+			//Send a message with the current scale, and send a message to block manage to shuffle the blocks!
 		}
-		/*if (Input.GetButtonDown("Fire1") && stopped)
+		else if (Input.GetButtonDown("Fire1") && stopped)
 		{
-			stopped = false;
-		}*/
+			//stopped = false;
+		}
 			
-		if (currentScale.y < 11.0f && growing && !stopped)
+		if (currentScale.y < (float) BlockManager.numOfBlocks - 0.05f && growing && !stopped)
 		{
 			newScale = gameObject.transform.localScale;
-			newScale.y = Mathf.Lerp(0.1f, 11.0f, t * 1.5f);
+			newScale.y = Mathf.Lerp(0.1f, 10.99f, t * 1.5f);
 			gameObject.transform.localScale = newScale;
 			t += Time.deltaTime;
 		}
-		if (currentScale.y > 10.9f && currentScale.y < 11.1f && growing && !stopped)
+		if (currentScale.y > (float) BlockManager.numOfBlocks - 0.1f &&
+			currentScale.y < (float) BlockManager.numOfBlocks + 0.1f && 
+			growing && !stopped)
 		{
 			growing = false;
 			t = 0;
@@ -49,7 +77,7 @@ public class BarGrowAndHit : MonoBehaviour {
 		if (currentScale.y > 0.0f && !growing && !stopped)
 		{
 			newScale = gameObject.transform.localScale;
-			newScale.y = Mathf.Lerp(11.0f, 0.1f, t * 1.5f);
+			newScale.y = Mathf.Lerp(10.99f, 0.1f, t * 1.5f);
 			gameObject.transform.localScale = newScale;
 			t += Time.deltaTime;
 		}
@@ -59,5 +87,33 @@ public class BarGrowAndHit : MonoBehaviour {
 			t = 0;
 		}
 		
+	}
+	
+	
+	
+	IEnumerator Flash () 
+	{
+		float t = 0;
+		while(t < flashRate)
+		{
+			blockChild.renderer.material.color = Color.Lerp(originalColor, Color.magenta, t/flashRate);
+			t += Time.deltaTime;
+			yield return null;
+		}
+		blockChild.renderer.material.color = Color.magenta;
+		StartCoroutine("Return");
+	}
+	
+	IEnumerator Return()
+	{
+		float x = 0;
+		while(x < flashRate)
+		{
+			blockChild.renderer.material.color = Color.Lerp(currentColor, originalColor, x/flashRate);// * 0.01f);
+			x += Time.deltaTime;
+			yield return null;
+		}
+		blockChild.renderer.material.color = originalColor;
+		//StartCoroutine("Flash");
 	}
 }
---
+++ FowlPlay/Assets/Scripts/ActionEventScripts/BlockManager.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+
+public class BlockManager : MonoBehaviour {
+	
+	//Like platform manager
+	public Transform prefab;
+	public static int numOfBlocks = 11;
+	public int numOfGreen = 1;
+	public Color aGreen = Color.green;
+	public int numOfYellow = 4;
+	public Color aYellow = Color.yellow;
+	public int numOfRed = 6;
+	public Color aRed = Color.red;
+	
+	private static Transform[] objectList;
+	private static Vector3 position;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("The total number of blocks is: " + numOfBlocks + "\n");
+		Debug.Log ("The number of colors is: " + (numOfGreen + numOfRed + numOfYellow) + "\n");
+		
+		objectList = new Transform[numOfBlocks];
+		for(int i = 0; i < numOfBlocks; i++)
+		{
+			objectList[i] = ((Transform)Instantiate(prefab));
+		}
+		for (int i = 0; i < numOfGreen; i++)
+		{
+			objectList[i].renderer.material.color = aGreen;
+		}
+		for (int i = numOfGreen; i < numOfGreen + numOfYellow; i++)
+		{
+			objectList[i].renderer.material.color = aYellow;
+		}
+		for (int i = numOfGreen + numOfYellow; i < numOfGreen + numOfYellow + numOfRed; i++)
+		{
+			objectList[i].renderer.material.color = aRed;
+		}
+		Shuffle();
+		PlaceBlocks();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	
+	public static void Shuffle()
+	{
+		Transform value;
+		//Random rng = new Random();  
+    	int n = numOfBlocks;  
+    	while (n > 1) 
+		{
+			n--;  
+   	     	int k = (int) Random.Range(0, numOfBlocks);  
+   	  	  	value = objectList[k];  
+   	   	  	objectList[k] = objectList[n];  
+   	     	objectList[n] = value;
+		}
+	}
+	
+	public static void PlaceBlocks()
+	{
+		//MAYBE MAKE IT A CHILD AND MAKE THESE THINGS RELATIVE
+		Vector3 newPlacement = new Vector3(1, 0, 5);
+		
+		for (int i = 0; i < numOfBlocks; i++)
+		{
+			newPlacement.y = (float) i + 0.5f;
+			objectList[i].localPosition = newPlacement;
+		}
+	}
+	
+	public static Transform CheckPosition(int scale)
+	{
+		Debug.Log("Color of the block at " + scale + " is: " + objectList[scale].renderer.material.color + "\n");
+		return objectList[scale];
+	}
+}
