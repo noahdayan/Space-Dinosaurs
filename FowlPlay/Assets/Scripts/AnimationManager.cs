e0c61e243ea35034d618616d9b185193f75b0295
Javier Pérez <javierp@lavabit.com>
2013-03-15 02:16:34 -0400

    Completed t-rex animation work

--- FowlPlay/Assets/Scripts/AnimationManager.cs
+++ FowlPlay/Assets/Scripts/AnimationManager.cs
@@ -9,15 +9,30 @@ public class AnimationManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!ClickAndMove.aIsObjectMoving && CharacterManager.aCurrentlySelectedUnit == gameObject)
+		
+		if(CharacterManager.aCurrentlySelectedUnit == gameObject)
+		{
+			if(!ClickAndMove.aIsObjectMoving && !CharacterManager.aInteractiveUnitIsSelected)
+				transform.FindChild("model").animation.Play("standing");
+			
+			else if(ClickAndMove.aIsObjectMoving)
+				transform.FindChild("model").animation.Play("walking");
+		
+			else if(CharacterManager.aMidTurn && CharacterManager.aInteractiveUnitIsSelected)
+				transform.FindChild("model").animation.Play("attack");
+		}
+			
+		else if(CharacterManager.aMidTurn)
+		{
+			if(!CharacterManager.aInteractiveUnitIsSelected)
+				transform.FindChild("model").animation.Play("standing");
+			
+			else if(CharacterManager.aInteractiveUnitIsSelected && CharacterManager.aInteractUnit == gameObject)
+				transform.FindChild("model").animation.Play("damage");
+		}
+		
+		else
 			transform.FindChild("model").animation.Play("standing");
-		else if(ClickAndMove.aIsObjectMoving && CharacterManager.aCurrentlySelectedUnit == gameObject)
-			transform.FindChild("model").animation.Play("walking");
-		else if(CharacterManager.aMidTurn && !CharacterManager.aInteractiveUnitIsSelected)
-			transform.FindChild("model").animation.Play("standing");
-		else if(CharacterManager.aMidTurn && CharacterManager.aInteractiveUnitIsSelected && CharacterManager.aCurrentlySelectedUnit == gameObject)
-			transform.FindChild("model").animation.Play("attack");
-		else if(CharacterManager.aMidTurn && CharacterManager.aInteractiveUnitIsSelected && CharacterManager.aInteractUnit == gameObject)
-			transform.FindChild("model").animation.Play("damage");
+
 	}
 }
