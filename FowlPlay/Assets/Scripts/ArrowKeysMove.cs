0eed06d47fcbf4b455732ec55684d8fb178a585c
Noah Dayan <noahdayan@gmail.com>
2013-03-16 20:19:05 -0400

    temp fix for camera movement

--- FowlPlay/Assets/Scripts/ArrowKeysMove.cs
+++ FowlPlay/Assets/Scripts/ArrowKeysMove.cs
@@ -18,87 +18,43 @@ public class ArrowKeysMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 newPosition = transform.position;
-		Vector3 tempPos = transform.position;
-		float temp;
+		Vector3 tempKey = Vector3.zero;
+		Vector3 tempMouse = Vector3.zero;
 		
-		// Time.deltaTime corrects for errors with the framerate, otherwise it's a pain.
-		temp = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		tempPos.x += temp;
-		tempPos.z += temp;
+		tempKey.z = Input.GetAxis("Vertical");
+		tempKey.y = Input.GetAxis("Vertical");
+    	tempKey.x = Input.GetAxis("Horizontal");
+		tempMouse.z = Input.GetAxis("Mouse ScrollWheel");
 		
-		//Checking range.
-		if(xRange.y < tempPos.x && tempPos.x < xRange.x)
-		{
-			newPosition.x += temp;
-		}
-		if(zRange.y < tempPos.z && tempPos.z < zRange.x)
-		{
-			newPosition.z += temp;
-		}
-		temp = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-		tempPos.z += temp;
-		tempPos.x -= temp;
-		if(zRange.y < tempPos.z && tempPos.z < zRange.x)
-		{
-			newPosition.z += temp;
-		}
-		if(xRange.y < tempPos.x && tempPos.x < xRange.x)
-		{
-			newPosition.x -= temp;
-		}
-		temp = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
-		tempPos.z += temp;
-		tempPos.x -= temp;
-		if(zRange.y < tempPos.z && tempPos.z < zRange.x)
-		{
-			newPosition.z += temp;
-		}
-		if(xRange.y < tempPos.x && tempPos.x < xRange.x)
-		{
-			newPosition.x -= temp;
-		}
-		temp = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
-		tempPos.y -= temp;
-		if(yRange.y < tempPos.y && tempPos.y < yRange.x)
-		{
-			newPosition.y -= temp;
-		}
-		transform.position = newPosition;
-			
+		transform.Translate(tempKey * speed * Time.deltaTime, Space.Self);
+		transform.Translate(tempMouse * scrollSpeed * Time.deltaTime, Space.Self);
+		
+		/*
 		//MOUSE MOVEMENT
 		//~~~HORIZONTAL~~~
 		//Move Camera Right, and check if it's in range
-		if ( Input.mousePosition.x >= Screen.width - mDelta 
-			/*&& xRange.y < transform.position.x*/ && transform.position.x < xRange.x
-			/*&& zRange.y < transform.position.z*/ && transform.position.z < zRange.x)
+		if ( Input.mousePosition.x >= Screen.width - mDelta && transform.position.x < xRange.x && transform.position.z < zRange.x)
 	    {
         	// Move the camera
         	transform.position += (Vector3.forward + Vector3.right) * Time.deltaTime * speed;
 		}
 		//Move Camera Left, and check if it's in range
-		if (Input.mousePosition.x <= mDelta 
-			&& xRange.y < transform.position.x /*&& transform.position.x < xRange.x*/
-			&& zRange.y < transform.position.z /*&& transform.position.z < zRange.x*/)
+		if (Input.mousePosition.x <= mDelta && xRange.y < transform.position.x && zRange.y < transform.position.z)
 		{
 			transform.position += (Vector3.back + Vector3.left) * Time.deltaTime * speed;
 		}
 		//~~~VERTICAL~~~
 		//Move Camera Up, and check if it's in range
-		if (Input.mousePosition.y >= Screen.height - mDelta
-			/*&& zRange.y < transform.position.z*/ && transform.position.z < zRange.x 
-			&& xRange.y < transform.position.x /*&& transform.position.x < xRange.x*/
-			)
+		if (Input.mousePosition.y >= Screen.height - mDelta && transform.position.z < zRange.x && xRange.y < transform.position.x)
 		{
 			transform.position += (Vector3.forward + Vector3.left) * Time.deltaTime * speed;
 		}
 		//Move Camera Down, and check if it's in range
-		if (Input.mousePosition.y < mDelta
-			&& zRange.y < transform.position.z /*&& transform.position.z < zRange.x*/
-			/*&& xRange.y < transform.position.x*/ && transform.position.x < xRange.x)
+		if (Input.mousePosition.y <= mDelta && zRange.y < transform.position.z && transform.position.x < xRange.x)
 		{
 			transform.position += (Vector3.back + Vector3.right) * Time.deltaTime * speed;
 		}	
+		*/
 		
 		if(Input.GetKey(KeyCode.Q))
 		{
