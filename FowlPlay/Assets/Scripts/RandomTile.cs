d1706a2d738a3f104224ee7091d72b0301929ce1
Noah Dayan <noahdayan@gmail.com>
2013-04-01 06:55:29 -0400

    mini game prefab tweak for rotations

--- FowlPlay/Assets/Scripts/RandomTile.cs
+++ FowlPlay/Assets/Scripts/RandomTile.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 using System.Collections;
 
 public class RandomTile : MonoBehaviour {
-
+	
+	public static bool isMiniGame = false;
+	
 	// Use this for initialization
 	void Start () {
-	transform.Rotate(Vector3.up, Random.Range(0, 6) * 60, Space.Self);
+		if(!isMiniGame)
+			transform.Rotate(Vector3.up, Random.Range(0, 6) * 60, Space.Self);
+		/*if(transform.Find("model"))
+		{
+			transform.Find("model").animation["standing"].time = Random.Range(0.0f, animation["standing"].length);
+		}*/
 	}
 	
 	// Update is called once per frame
