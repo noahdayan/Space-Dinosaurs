b792779ba41673f9bf800babd58af73bd55537bf
Javier Pérez <javierp@lavabit.com>
2013-04-01 23:24:31 -0400

    ItemManager fixed to handle static items and no longer handle random spawning. ItemManager must be enabled in Character in the hierarchy.

--- FowlPlay/Assets/Scripts/ItemManager.cs
+++ FowlPlay/Assets/Scripts/ItemManager.cs
@@ -5,53 +5,17 @@ public class ItemManager : MonoBehaviour {
 	
 	public static Hashtable tilesWithItems;
 	
-	public GameObject dinoCoOil, birdSeed, dinoChow;
-	
-	private GameObject[] prefabs;
-	
 	// Use this for initialization
 	void Start () {
-		tilesWithItems = new Hashtable();
-		prefabs = new GameObject[] {dinoCoOil, birdSeed, dinoChow};
-		StartCoroutine("SpawnItems");
-	}
-	
-	IEnumerator SpawnItems()
-	{
-		// Necessary to wait for a second before proceeding because TileManager needs to start-up first.
-		yield return new WaitForSeconds(0.5f);
 		
-		// Spawn some items
-		for (int i = 0; i < 5; i++)
+		tilesWithItems = new Hashtable();
+		
+		foreach (GameObject item in GameObject.FindGameObjectsWithTag("Item"))
 		{
-			GameObject tile;
-			
-			do
-				 tile = TileManager.TruePickRandomTile();
-			while (tilesWithItems.Contains(tile.transform.position));
-			
-			GameObject itemObject = prefabs[Random.Range(0, prefabs.Length)];
-			// Set position
-			Vector3 itemPosition = tile.transform.position;
-			itemPosition.y = itemObject.transform.position.y;
-			
-			// Instantiate (put on map) and add to hashtable
-			GameObject item = (GameObject)Instantiate(itemObject, itemPosition, itemObject.transform.rotation);
-			tilesWithItems.Add(tile.transform.position, item);
-			
-			switch (item.name)
-			{
-				case "DinoChow(Clone)" :
-					item.tag = "DinoChow";
-					break;
-				case "BirdSeed(Clone)" :
-					item.tag = "BirdSeed";
-					break;
-				case "DinoCoOil(Clone)" :
-					item.tag = "DinoCoOil";
-					break;
-			}
-			
+			// Get the tile the item is standing on.
+			Vector3 tile = TileManager.getTileUnitIsStandingOn(item);
+
+			tilesWithItems.Add(tile, item);
 		}
 		
 	}
